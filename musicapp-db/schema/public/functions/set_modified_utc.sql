d0cebd3c72de882b85a8ad87ede2787db9dd4955
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-05-25 18:28:40 -0400

    Moving header into function

--- musicapp-db/schema/public/functions/set_modified_utc.sql
+++ musicapp-db/schema/public/functions/set_modified_utc.sql
@@ -1,17 +1,18 @@
-/*
--- ======================================================
--- Author:        Carlos Gonzalez
--- Date Created:  2026-05-09
--- Description:   Updates rows when modified
----------------------------------------------------------
--- YYYY-MM-DD - Author - Change
--- 
--- ======================================================
-*/
-
 CREATE OR REPLACE FUNCTION set_modified_utc()
 RETURNS TRIGGER AS $$
 BEGIN
+
+    /*
+    -- ======================================================
+    -- Author:        Carlos Gonzalez
+    -- Date Created:  2026-05-09
+    -- Description:   Updates rows when modified
+    ---------------------------------------------------------
+    -- YYYY-MM-DD - Author - Change
+    -- 
+    -- ======================================================
+    */
+
     NEW.modified_utc = NOW() AT TIME ZONE 'UTC';
     RETURN NEW;
 END;
